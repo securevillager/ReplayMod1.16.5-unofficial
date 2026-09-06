400cd865a5343c5b3bf2b641ca2ed58baf4ad615
securevillager <293481871+securevillager@users.noreply.github.com>
2026-09-06 12:14:02 +0900

    Refactor perspective method parameters and logic
    Refactor replayModRender_perspective method for clarity and consistency.

--- src/main/java/com/replaymod/mixin/Mixin_Omnidirectional_Camera.java
+++ src/main/java/com/replaymod/mixin/Mixin_Omnidirectional_Camera.java
@@ -9,16 +9,17 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(GameRenderer.class)
 public abstract class Mixin_Omnidirectional_Camera implements EntityRendererHandler.IEntityRenderer {
-    @Redirect(method = "method_22973", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/vector/Matrix4f;perspective(DFFF)Lnet/minecraft/util/math/vector/Matrix4f;"))
+    @Redirect(method = "method_22973", at = @At(value = "INVOKE", target ="Lnet/minecraft/util/math/vector/Matrix4f;perspective(FFFF)Lnet/minecraft/util/math/vector/Matrix4f;"
     private Matrix4f replayModRender_perspective$0(double fovY, float aspect, float zNear, float zFar) {
         return replayModRender_perspective((float) fovY, aspect, zNear, zFar);
     }
 
-    private Matrix4f replayModRender_perspective(float fovY, float aspect, float zNear, float zFar) {
-        if (replayModRender_getHandler() != null && replayModRender_getHandler().omnidirectional) {
-            fovY = 90;
-            aspect = 1;
-        }
-        return Matrix4f.perspective(fovY, aspect, zNear, zFar);
-    }
+    private Matrix4f replayModRender_perspective$0(
+        float fovY,
+        float aspect,
+        float zNear,
+        float zFar
+    ) {
+    return replayModRender_perspective(fovY, aspect, zNear, zFar);
+ }
 }
